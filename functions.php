371d8df65295e97dd6821fcfca164e907003114b
Gunnar Hellekson <gunnar@hellekson.com>
2012-10-23 16:42:42 +0000

    Added support for showing thumbs properly on sticky posts. Added new "sticky-thumb" image size.

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
 add_theme_support( 'post-formats', array( 'image', 'status', 'gallery', 'link', 'quote' ) );
 
+if ( function_exists( 'add_theme_support' ) ) {
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 200, 9999 ); // default Post Thumbnail dimensions   
+}
+
+if ( function_exists( 'add_image_size' ) ) { 
+	add_image_size( 'sticky-thumb', 200, 9999 ); //200 pixels wide (and unlimited height)
+}
+
 /*
  * Do cleanup on some transient values in the database.
  */
